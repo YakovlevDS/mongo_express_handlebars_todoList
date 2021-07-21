89bef60538b114b2dc40c5201d7b2596671fac89
Dmitriy Yakovlev <58001401+YakovlevDS@users.noreply.github.com>
2021-07-21 18:11:44 +0300

    added base files

---
+++ app.js
@@ -0,0 +1,11 @@
+// const http= require('http');
+// // http.createServer().listen(3000);
+// http.createServer(function(req,res){
+//     console.log(req.url);
+//     console.log(req.method);
+//     console.log(req.headers['user-agent']);
+//     res.setHeader("Content-Type","text/html; charset=utf8;");
+//     if (req.url=='/'){res.end('Main <b>sdfsf</b> sdfdfg');} 
+//     if (req.url=='/cat'){res.end('Category <h1>sdfsf</h1> вапр')}
+// }
+// ).listen(3000);
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect('mongodb+srv://user:user@cluster0.9csta.mongodb.net/todos' ,
+    {
+        useNewUrlParser:true,
+        useFindAndModify:false,
+        useUnifiedTopology: true
+    })
+    
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
